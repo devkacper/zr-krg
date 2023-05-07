29eb33ecdff640cdf228bc90d44bb790d4d56c3b
kacper <kacperlstasiak@gmail.com>
2023-05-07 21:32:26 +0200

    Poprawiono domyślne sortowanie rekordów walutowych według daty utworzenia.

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Currency extends Model
 {
-    use HasFactory;
+    /**
+     * Sort Currency records by created_at DESC.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('created_at', 'desc');
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
